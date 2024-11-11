092a65808e12a8d6f91f8e54bd2e20d98fac2422
Paul Popus <paul@nouance.io>
2024-11-11 01:50:53 -0600

    added server url to payload confgi

--- api/src/payload.config.ts
+++ api/src/payload.config.ts
@@ -27,8 +27,10 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
+  serverURL: process.env.SERVER_URL || 'http://localhost:3000',
   admin: {
     user: Users.slug,
+
     importMap: {
       baseDir: path.resolve(dirname),
     },
